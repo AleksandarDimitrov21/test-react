9414626b12d0e1ebdfbb76f9c0747f0b50e7c16e
AleksandarDimitrov21 <165321350+AleksandarDimitrov21@users.noreply.github.com>
2024-03-30 13:22:55 +0200

    CartPage
    Golqm div koyto ima div za itemite i drug za summaryto

--- my-project/src/components/ui/Product.jsx
+++ my-project/src/components/ui/Product.jsx
@@ -3,17 +3,21 @@ import React from "react";
 function Product(props) {
   const { id, productName, price, productImage } = props.data;
   return (
-    <div className="product w-64 h-64 overflow-hidden">
-      <div>
+    <div className="product h-auto flex items-center border-2 border-black border-solid mb-5">
+      <div className="w-3/12">
         <img
           src={productImage}
           alt={productName}
           className="h-full w-full object-cover object-center"
         />
+        <hr className="border-t-2 border-gray-300"></hr>
       </div>
-      <div className="p-4">
-        <h2 className="text-xl font-bold text-black">{productName}</h2>
-        <p className="text-lg font-semibold text-black">${price}</p>
+      <div className="flex-1 p-4">
+        <div className="flex-1 p-4 flex items-center">
+          <h2 className="text-lg font-medium text-black mr-2">{productName}</h2>
+          <p className="text-lg font-medium text-black ml-auto">${price}</p>
+        </div>
+        <hr className="border-t-2 border-gray-300"></hr>
       </div>
     </div>
   );
--- my-project/src/pages/CartPage.jsx
+++ my-project/src/pages/CartPage.jsx
@@ -13,14 +13,43 @@ const CartPage = () => {
           </button>
         </Link>
         <div className="text-black font-bold text-4xl">CartPage</div>
-        <div className="cart">
-          <div>
-            <h1>Your Cart Items</h1>
+        <div className="flex w-full justify-center h-screen">
+          <div className="cart w-2/5">
+            <div>
+              <h1 className="text-black text-2xl font-bold mb-4 mt-12">Bag</h1>
+            </div>
+            <div className="cart-items">
+              {PRODUCTS.map((product) => {
+                return <Product key={product.id} data={product} />;
+              })}
+            </div>
           </div>
-          <div className="cart">
-            {PRODUCTS.map((product) => {
-              return <Product key={product.id} data={product} />;
-            })}
+          <div className="summary ml-8 w-80">
+            <div>
+              <h1 className="text-black text-2xl font-bold mb-4 mt-12">
+                Summary
+              </h1>
+            </div>
+            <div className="summary-text">
+              <p className="text-lg font-medium text-black mb-3">
+                Subtotal:&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp; 1230$
+              </p>
+              <p className="text-lg font-medium text-black mb-6">
+                Estimated Delivery & Handling: &nbsp; &nbsp; Free
+              </p>
+              <hr className="border-t-2 border-gray-300"></hr>
+              <p className="text-lg font-medium text-black mb-6 mt-5">
+                Total:&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 1230$
+              </p>
+              <hr className="border-t-2 border-gray-300"></hr>
+              <button className="bg-black hover:bg-zinc-700 text-white font-semibold py-5 px-5 text-lg rounded-full w-80 mt-5">
+                Checkout
+              </button>
+            </div>
           </div>
         </div>
       </div>
