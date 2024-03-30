86f8a84e676974d541d205b22371ebda83beb6c3
AleksandarDimitrov21 <165321350+AleksandarDimitrov21@users.noreply.github.com>
2024-03-30 14:13:16 +0200

    Nishto

--- my-project/src/pages/CartPage.jsx
+++ my-project/src/pages/CartPage.jsx
@@ -1,10 +1,52 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CartPage = () => {
   return (
-    <div className="h-screen bg-white">
-      <div className="text-black font-bold text-4xl">CartPage</div>
-    </div>
+    <>
+      <div className="h-screen bg-white">
+        <Link to="/">
+          <button className="absolute top-0 right-0 m-4 text-lg text-black font-bold">
+            Home
+          </button>
+        </Link>
+        <div className="text-black font-bold text-4xl">CartPage</div>
+        <div className="flex w-full justify-center h-screen">
+          <div className="cart w-2/5">
+            <div>
+              <h1 className="text-black text-2xl font-bold mb-4 mt-12">Bag</h1>
+            </div>
+          </div>
+          <div className="summary ml-8 w-80">
+            <div>
+              <h1 className="text-black text-2xl font-bold mb-4 mt-12">
+                Summary
+              </h1>
+            </div>
+            <div className="summary-text">
+              <p className="text-lg font-medium text-black mb-3">
+                Subtotal:&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp; 1230$
+              </p>
+              <p className="text-lg font-medium text-black mb-6">
+                Estimated Delivery & Handling: &nbsp; &nbsp; Free
+              </p>
+              <hr className="border-t-2 border-gray-300"></hr>
+              <p className="text-lg font-medium text-black mb-6 mt-5">
+                Total:&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 1230$
+              </p>
+              <hr className="border-t-2 border-gray-300"></hr>
+              <button className="bg-black hover:bg-zinc-700 text-white font-semibold py-5 px-5 text-lg rounded-full w-80 mt-5">
+                Checkout
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
