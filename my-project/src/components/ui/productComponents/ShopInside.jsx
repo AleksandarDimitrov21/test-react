be31fa13e4aaf678debd509f17acfc3020ee4704
Angel Stoynov <an.stoynov@gmail.com>
2024-04-05 22:01:30 +0300

    Added Buttons to ShopInside

--- my-project/src/components/ui/productComponents/ShopInside.jsx
+++ my-project/src/components/ui/productComponents/ShopInside.jsx
@@ -1,8 +1,8 @@
-import React from "react";
 import NavBar from "../navigation/NavBar";
+import { useState } from "react";
 
 const ShopInside = ({ status, setStatus }) => {
-  const [smallImages, setSmallImages] = React.useState([
+  const [smallImages, setSmallImages] = useState([
     {
       url: "https://image-us.samsung.com/SamsungUS/home/television-home-theater/tvs/samsung-neo-qled-8k/03162023/QN65QN900CFXZA_003_L-Perspective_Titan-Black-1600x1200-1.jpg?$product-details-jpg$",
       selected: false,
@@ -17,8 +17,8 @@ const ShopInside = ({ status, setStatus }) => {
     },
   ]);
 
-  const [mainImageHeight, setMainImageHeight] = React.useState(400);
-
+  const [mainImageHeight, setMainImageHeight] = useState(400);
+  const [showButtons, setShowButtons] = useState(true);
   const handleImageClick = (index) => {
     setSmallImages(
       smallImages.map((image, i) => {
@@ -32,55 +32,89 @@ const ShopInside = ({ status, setStatus }) => {
   };
 
   return (
-    <div className=" min-h-screen bg-white">
-      <div className="overflow-hidden h-16 ">
-        <NavBar isLoggedIn={status} setIsLoggedIn={setStatus} />
-      </div>
-      <div className="flex justify-betwwen items-center mt-32">
-        <div className="w-1/2 flex flex-col items-center justify-center border-r-2 border-gray-300">
-          <img
-            src={
-              (smallImages.find((image) => image.selected) || smallImages[0])
-                ?.url ||
-              "https://www.smart-house.bg/4986-thickbox_default/smart-robotic-vacuum-cleaner.jpg"
-            }
-            alt="Product Image"
-            className="w-full h-auto max-w-md mb-4"
-            style={{ height: `${mainImageHeight}px`, objectFit: "cover" }}
-          />
-          <div className="flex flex-wrap w-full justify-center">
-            {smallImages.map((image, index) => (
-              <img
-                key={index}
-                src={image.url}
-                alt={`Small image ${index}`}
-                className={`w-1/3 h-auto object-cover object-center cursor-pointer ${
-                  image.selected
-                    ? "border-2 border-gray-400"
-                    : "hover:border-2 hover:border-gray-300"
-                }`}
-                onClick={() => handleImageClick(index)}
-                style={{ maxHeight: "120px", maxWidth: "80px", margin: "4px" }}
-              />
-            ))}
+    <>
+      <div className=" min-h-screen bg-white">
+        <div className="overflow-hidden h-16 ">
+          <NavBar isLoggedIn={status} setIsLoggedIn={setStatus} />
+        </div>
+        <div className="flex justify-betwwen items-center mt-32">
+          <div className="w-1/2 flex flex-col items-center justify-center border-r-2 border-gray-300">
+            <img
+              src={
+                (smallImages.find((image) => image.selected) || smallImages[0])
+                  ?.url ||
+                "https://www.smart-house.bg/4986-thickbox_default/smart-robotic-vacuum-cleaner.jpg"
+              }
+              alt="Product Image"
+              className="w-full h-auto max-w-md mb-4"
+              style={{ height: `${mainImageHeight}px`, objectFit: "cover" }}
+            />
+            <div className="flex flex-wrap w-full justify-center">
+              {smallImages.map((image, index) => (
+                <img
+                  key={index}
+                  src={image.url}
+                  alt={`Small image ${index}`}
+                  className={`w-1/3 h-auto object-cover object-center cursor-pointer ${
+                    image.selected
+                      ? "border-2 border-gray-400"
+                      : "hover:border-2 hover:border-gray-300"
+                  }`}
+                  onClick={() => handleImageClick(index)}
+                  style={{
+                    maxHeight: "120px",
+                    maxWidth: "80px",
+                    margin: "4px",
+                  }}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col justify-center pt-16 pb-16 p-32 rounded-xl ml-16 bg-gray-200">
+            <div className="flex justify-between items-center mb-4">
+              <h2 className="text-3xl px-20 text-black">Product Name</h2>
+              {showButtons && (
+                <>
+                  <button
+                    className="rounded-full bg-red-500 text-white p-2 mr-2"
+                    onClick={() => {
+                      console.log("Delete button clicked");
+                    }}
+                  >
+                    <img
+                      width={20}
+                      height={20}
+                      src="/delete.svg"
+                      alt="delete"
+                    />
+                  </button>
+                  <button
+                    className="rounded-full bg-violet-500 text-white p-2 mr-2"
+                    onClick={() => {
+                      console.log("Edit button clicked");
+                    }}
+                  >
+                    <img width={20} height={20} src="/edit.svg" alt="edit" />
+                  </button>
+                </>
+              )}
+              <div className="flex space-x-4"></div>
+            </div>
+            <p className="text-slate-800">Kod: 123123</p>
+            <p className="text-slate-800 mb-4">In stock</p>
+            <p className="mb-4 text-slate-900 text-xl">
+              Harakteristiki za produkta
+            </p>
+            <p className="text-2xl mb-2 text-black">
+              Price: <span className="text-xl">BGN 1289.00</span>
+            </p>
+            <button className="mt-8 bg-violet-500 hover:bg-violet-400 text-white font-bold py-2 px-4 rounded-xl">
+              Add to Cart
+            </button>
           </div>
         </div>
-        <div className="flex flex-col justify-center pt-16 pb-16 p-32 rounded-xl  ml-16 bg-gray-200">
-          <h2 className="text-3xl px-20 mb-4 text-black">Product Name</h2>
-          <p className="text-slate-800 ">Kod: 123123</p>
-          <p className="text-slate-800 mb-4">In stock</p>
-          <p className="mb-4 text-slate-900 text-xl">
-            Harakteristiki za produkta
-          </p>
-          <p className="text-2xl mb-2 text-black">
-            Price: <span className="text-xl">BGN 1289.00</span>
-          </p>
-          <button className="mt-8 bg-violet-500 hover:bg-violet-400 text-white font-bold py-2 px-4 rounded-xl">
-            Add to Cart
-          </button>
-        </div>
       </div>
-    </div>
+    </>
   );
 };
 
